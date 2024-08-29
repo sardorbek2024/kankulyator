d310eac59a4163ac1cd721d4fc0933dedbd49359
sardorbek2024 <168269584+sardorbek2024@users.noreply.github.com>
2024-08-29 11:08:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let a = +prompt("birinchi soni kiriting");
+let b = prompt("qiymat kiriting");
+let c = +prompt("ikkinchi soni kiriting");
+
+
+if(b=="+") {
+    alert(a + c)
+}
+
+if(b=="*") {
+    alert(a * c)
+}
+
+if(b=="-") {
+    alert(a - c)
+}
+
+if(b=="/") {
+    alert(a / c)
+}
